6ad8e8d129f9b8615a058ad018ab0a9be476bfd9
mukitcoder <mukitbd71@gmail.com>
2021-10-27 21:44:29 +0600

    ok

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 const express = require("express");
+const cors = require("cors")
+
+
 const app = express();
+
+
+app.use(cors())
+app.use(express.json())
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send(" hello from my node");
 });
 
+
 const users = [
-    {id:1 , name: 'Shabana', email: 'shabana@gmail.com', phone: '0125554555'},
-    {id:2 , name: 'Bobi', email: 'sna@gmail.com', phone: '0125554555'},
-    {id:3 , name: 'kobi', email: 'koni@gmail.com', phone: '0125554555'},
-    {id:4 , name: 'Naila', email: 'naila@gmail.com', phone: '0125554555'},
-    {id:5 , name: 'Saila', email: 'Saila@gmail.com', phone: '0125554555'},
-]
+  { id: 1, name: "Shabana", email: "shabana@gmail.com", phone: "0125554555" },
+  { id: 2, name: "Bobi", email: "sna@gmail.com", phone: "0125554555" },
+  { id: 3, name: "kobi", email: "koni@gmail.com", phone: "0125554555" },
+  { id: 4, name: "Naila", email: "naila@gmail.com", phone: "0125554555" },
+  { id: 5, name: "Saila", email: "Saila@gmail.com", phone: "0125554555" },
+];
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  const search = req.query.search;
+  if (search) {
+    const searchResult = users.filter((user) =>
+      user.name.toLocaleLowerCase().includes(search)
+    );
+    res.send(searchResult);
+  } else {
+    res.send(users);
+  }
 });
 
-app.get('/users/:id', (req, res)=>{
-    const id = req.params.id
-    const user = users[id]
-    res.send(user)
+
+
+app.post("/users", (req, res)=>{
+    const newUser = req.body;
+    newUser.id = users.length
+    users.push(newUser)
+    // res.send('inside post')
+    res.json(newUser)
 })
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users[id];
+  res.send(user);
+});
 
 app.listen(port, () => {
   console.log("listening to port ", port);
